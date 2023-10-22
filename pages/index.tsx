14f16b20422d60a4ddc2f66ef04fd7d8693d3075
isaacj32 <isaacj32@hotmail.com>
2023-10-21 20:25:07 -0500

    Mapa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
+"use client";
 import { ExampleComponent } from '@/components/ExampleComponent';
 import { Footer } from '@/components/common/footer';
-import Map from '@/components/map';
+import dynamic from "next/dynamic";
+const DynamicMapComponent = dynamic(() => import("@/components/MapComponent"), { ssr: false });
 
 const Home = () => {
   return (
     <div>
       <main className='flex h-screen w-full items-center justify-center'>
-        <Map/>
+        <ExampleComponent text='Welcome to CodeF@ctory' />
+        <DynamicMapComponent />
       </main>
       <Footer />
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -17,4 +17,5 @@ const config: Config = {
   },
   plugins: [],
 }
+// eslint-disable-next-line import/no-default-export
 export default config
