b4fe96281bb296f86bd2608a788382334522eb50
unknown <andres.pena2@udea.edu.co>
2023-10-24 20:59:23 -0500

    Validacion errores HU Politicas de pago

--- components/GestionFinanciera/Footer.tsx
+++ components/GestionFinanciera/Footer.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export const Footer = () => {
   return (
     <footer
-      className='md:fixed w-full bg-sky-600 bottom-0 flex justify-between max-sm:flex-col items-start 
-    max-sm:items-center text-center self-stretch pt-9 pb-14 md:pr-36 md:pl-12 sm:pr-24 sm:pl-8 px-8 gap-9'
-    >
+  className='md:fixed w-full bg-sky-600 bottom-0 flex justify-between max-sm:flex-col items-start 
+  max-sm:items-center text-center self-stretch pt-4 pb-4 md:pr-36 md:pl-12 sm:pr-24 sm:pl-8 px-8 gap-9'
+>
       <div className='flex flex-col gap-2'>
         <div className='text-white font-semibold text-4xl'>SSMU</div>
         <div className='text-slate-300 pt-4'>
