f2f32b4ff8235908081f2c2479c7f0b346ac1b45
Sneyderbg <sneyder.buitrago@udea.edu.co>
2023-10-29 14:08:24 -0500

    feat: visual design and responsiveness improved

--- pages/GestionFinanciera/Tarifas/PorCiudad/index.tsx
+++ pages/GestionFinanciera/Tarifas/PorCiudad/index.tsx
@@ -2,7 +2,9 @@ import {
   actualizarTarifaPorCiudad,
   obtenerTarifasPorCiudad,
 } from '@/api/GestionFinanciera/Tarifas';
+import { GrowWrapper } from '@/components/GestionFinanciera/GrowWrapper';
 import { Layout } from '@/components/GestionFinanciera/Layout';
+import { SkeletonWrapper } from '@/components/GestionFinanciera/SkeletonWrapper';
 import {
   CityFeeRequest,
   CityFeeResponse,
@@ -12,6 +14,7 @@ import {
   Autocomplete,
   Button,
   Grid,
+  Grow,
   LinearProgress,
   Paper,
   TextField,
@@ -24,6 +27,9 @@ import { NumericFormat } from 'react-number-format';
 const PorCiudadPage = () => {
   const [submitStatus, setSubmitStatus] = useState<
     'pending' | 'success' | 'error' | undefined
+  >(undefined);
+  const [fetchStatus, setFetchStatus] = useState<
+    'success' | 'pending' | 'error' | undefined
   >(undefined);
   const [citiesMap, setCitiesMap] = useState<
     Map<string, { cityId: number; percentage: number }>
@@ -40,20 +46,31 @@ const PorCiudadPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    obtenerTarifasPorCiudad().then((res) => {
-      if (res.status == 200) {
-        const data: CityFeeResponse = res.data;
-        const map = new Map(
-          data.map((cityFee) => {
-            return [
-              cityFee.name,
-              { cityId: cityFee.id, percentage: cityFee.percentage },
-            ];
-          })
-        );
-        setCitiesMap(map);
-      }
-    });
+    setSubmitStatus(undefined);
+    setFetchStatus('pending');
+    setTimeout(() =>
+      obtenerTarifasPorCiudad()
+        .then((res) => {
+          if (res.status == 200) {
+            const data: CityFeeResponse = res.data;
+            const map = new Map(
+              data.map((cityFee) => {
+                return [
+                  cityFee.name,
+                  { cityId: cityFee.id, percentage: cityFee.percentage },
+                ];
+              })
+            );
+            setCitiesMap(map);
+            setFetchStatus('success');
+          }
+        })
+        .catch((err) => {
+          setFetchStatus('error');
+          // eslint-disable-next-line no-console
+          console.error(err.toJSON());
+        })
+    );
   }, []);
 
   useEffect(() => {
@@ -114,86 +131,123 @@ const PorCiudadPage = () => {
 
   return (
     <Layout>
-      <Paper
-        component='form'
-        elevation={4}
-        className='p-6'
-        onSubmit={handleSubmit}
-      >
-        <Grid container direction='column' spacing={{ sm: 4, xs: 2 }}>
-          <Grid item className='flex justify-center'>
-            <Typography variant='subtitle1'>TARIFAS POR CIUDAD</Typography>
-          </Grid>
-          <Grid item className='flex justify-between items-center gap-4'>
-            <Typography variant='body1' className='whitespace-nowrap'>
-              Elegir ciudad:
-            </Typography>
-            <Autocomplete
-              id='city'
-              value={formData.city}
-              options={Array.from(citiesMap.keys())}
-              noOptionsText=''
-              size='small'
-              className='w-1/2'
-              renderInput={(params) => (
-                <TextField
-                  required
-                  name='city'
-                  variant='filled'
-                  label='Ciudad'
-                  sx={{ width: '100%' }}
-                  {...params}
+      <Grow in>
+        <Paper
+          component='form'
+          elevation={4}
+          className='p-6'
+          onSubmit={handleSubmit}
+        >
+          <Grid container direction='column' spacing={{ sm: 4, xs: 2 }}>
+            <Grid item className='flex justify-center'>
+              <SkeletonWrapper
+                className='w-60'
+                loading={fetchStatus == 'pending'}
+              >
+                <Typography variant='subtitle1'>TARIFAS POR CIUDAD</Typography>
+              </SkeletonWrapper>
+            </Grid>
+            <Grid item className='flex justify-between items-center gap-4'>
+              <SkeletonWrapper loading={fetchStatus == 'pending'}>
+                <Typography variant='body1' className='whitespace-nowrap'>
+                  Elegir ciudad:
+                </Typography>
+                <Autocomplete
+                  id='city'
+                  value={formData.city}
+                  options={Array.from(citiesMap.keys())}
+                  noOptionsText=''
+                  size='small'
+                  className='w-1/2'
+                  renderInput={(params) => (
+                    <TextField
+                      required
+                      name='city'
+                      variant='filled'
+                      label='Ciudad'
+                      sx={{ width: '100' }}
+                      {...params}
+                    />
+                  )}
+                  onChange={(_evt, newCity: string | null) => {
+                    handleChange({
+                      target: { name: 'city', value: newCity },
+                    });
+                  }}
                 />
+              </SkeletonWrapper>
+            </Grid>
+            <Grid item className='flex justify-between items-center gap-8'>
+              <SkeletonWrapper loading={fetchStatus == 'pending'}>
+                <Typography variant='body1' className='whitespace-nowrap'>
+                  Porcentaje de tarifa:
+                </Typography>
+                <NumericFormat
+                  id='percentage'
+                  name='percentage'
+                  label='%'
+                  value={formData.percentage}
+                  required
+                  error={errors.percentage.length > 0}
+                  helperText={errors.percentage}
+                  variant='filled'
+                  size='small'
+                  className='w-1/2'
+                  customInput={TextField}
+                  onChange={handleChange}
+                />
+              </SkeletonWrapper>
+            </Grid>
+            {/* Alerts section */}
+            <Grid item className='flex justify-center self-stretch'>
+              {submitStatus == 'success' && (
+                <Alert severity={'success'}>Guardado éxitoso</Alert>
               )}
-              onChange={(_evt, newCity: string | null) => {
-                handleChange({
-                  target: { name: 'city', value: newCity },
-                });
-              }}
-            />
+              {submitStatus == 'error' && (
+                <Alert severity='error'>No se pudo actualizar la tarifa</Alert>
+              )}
+              {submitStatus == 'pending' && (
+                <LinearProgress className='w-full'></LinearProgress>
+              )}
+              {fetchStatus == 'error' && (
+                <Alert severity='error'>
+                  No se puede conectar con el servidor, inténtelo más tarde
+                </Alert>
+              )}
+            </Grid>
+            <Grid item className='flex justify-evenly gap-2'>
+              {fetchStatus == 'pending' ? (
+                <>
+                  <SkeletonWrapper
+                    className='w-full'
+                    loading={fetchStatus == 'pending'}
+                  ></SkeletonWrapper>
+                </>
+              ) : (
+                <GrowWrapper>
+                  <Button
+                    disabled={
+                      fetchStatus == 'error' || submitStatus == 'pending'
+                    }
+                    variant='contained'
+                    size='large'
+                    type='submit'
+                  >
+                    GUARDAR
+                  </Button>
+                  <Button
+                    variant='contained'
+                    size='large'
+                    onClick={() => router.push('/GestionFinanciera/Tarifas')}
+                  >
+                    REGRESAR
+                  </Button>
+                </GrowWrapper>
+              )}
+            </Grid>
           </Grid>
-          <Grid item className='flex justify-between items-baseline gap-4'>
-            <Typography variant='body1' className='whitespace-nowrap'>
-              Porcentaje de tarifa:
-            </Typography>
-            <NumericFormat
-              id='percentage'
-              name='percentage'
-              label='%'
-              value={formData.percentage}
-              required
-              error={errors.percentage.length > 0}
-              helperText={errors.percentage}
-              variant='filled'
-              size='small'
-              className='w-1/2'
-              customInput={TextField}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item>
-            {submitStatus == 'success' && (
-              <Alert severity={'success'}>Guardado éxitoso</Alert>
-            )}
-            {submitStatus == 'error' && (
-              <Alert severity='error'>No se pudo actualizar la tarifa</Alert>
-            )}
-            {submitStatus == 'pending' && <LinearProgress></LinearProgress>}
-          </Grid>
-          <Grid item className='flex justify-evenly gap-2'>
-            <Button variant='contained' size='large' type='submit'>
-              GUARDAR
-            </Button>
-            <Button
-              variant='contained'
-              size='large'
-              onClick={() => router.push('/GestionFinanciera/Tarifas')}
-            >
-              REGRESAR
-            </Button>
-          </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </Grow>
     </Layout>
   );
 };
