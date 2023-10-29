bef141029f4f6f6f19e60b5a5c991fcdc926661c
unknown <andres.pena2@udea.edu.co>
2023-10-28 19:01:38 -0500

    Aplicacion de MaterialUI a la historia de usuario EstadoDeCuenta

--- pages/GestionFinanciera/GestionDeCuenta/EstadoDeCuenta/index.tsx
+++ pages/GestionFinanciera/GestionDeCuenta/EstadoDeCuenta/index.tsx
@@ -1,12 +1,27 @@
 import { Layout } from '@/components/GestionFinanciera/Layout';
-import { Button } from '@/components/GestionFinanciera/Button';
-
-
+import * as React from 'react';
+import { Button, Grid,TableContainer,Table,TableHead,TableCell,TableRow,TableBody,Paper,Typography } from '@mui/material';
 
 import { useRouter } from 'next/router';
 
 
+function createData(
+  Usuario: string,
+  FechayHora: String,
+  Valor: String,
+  Estado: String,
+  
+) {
+  return { Usuario, FechayHora, Valor, Estado };
+}
 
+const rows = [
+  createData('Juan', "10-10-23 11:05", "$14000", "Por liquidar" ),
+  createData('Jose'," 10-10-23 01:05", "$5555", "Por liquidar"),
+  createData('David', "10-1-23 11:05", "$5554242", "Por liquidar"),
+  createData('Andrés', "9-10-21 11:05", "$34234"," Por liquidar"),
+  createData('Daniel', "1-8-22 11:05", "$2131321", "Por liquidar"),
+];
 
 
 
@@ -18,55 +33,55 @@ const EstadoDeCuentaPage = () => {
     const router = useRouter();
   
     return (
-        <Layout >
-                  <div>
-        
-                  <legend className="px-4 py-8" style={{ fontSize: '24px', fontWeight: 'bold', marginLeft: '250px',marginTop: '-100px' }}>Estado de cuenta</legend>
+      <Layout>
+        <div style={{ display: 'flex',flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', marginTop:-60,gap: '30px' }}>
+        <Typography variant='subtitle1' fontWeight={800}>
+              TARIFAS DE TRANSPORTE SIN PARADA
+            </Typography>
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+        <TableRow>
+    <TableCell><strong>USUARIO</strong></TableCell>
+    <TableCell align="right"><strong>FECHA Y HORA</strong></TableCell>
+    <TableCell align="right"><strong>VALOR&nbsp;(g)</strong></TableCell>
+    <TableCell align="right"><strong>ESTADO&nbsp;(g)</strong></TableCell>
+</TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.Usuario}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.Usuario}
+              </TableCell>
+              <TableCell align="right">{row.FechayHora}</TableCell>
+              <TableCell align="right">{row.Valor}</TableCell>
+              <TableCell align="right">{row.Estado}</TableCell>
+              
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+       <Grid item>
+          <Button
+            variant='contained'
+            size='large'
+            className='w-full'
+            onClick={() => router.push('/GestionFinanciera/GestionDeCuenta')}
+          >
+            REGRESAR
+          </Button>
+        </Grid>
+        </div>
+    </Layout>
+  );
+};
 
 
 
-                  <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-      
-      <div className="p-4">
-        <h2 className="mb-4" style={{ fontSize: '20px', fontWeight: 'bold'}} >USUARIO</h2>
-        <p className="mb-4">pepito123</p>
-        <p className="mb-4">juanito24</p>
-        <p className="mb-4">cesar35</p>
-      </div>
-
-   
-      <div className="p-4">
-        <h2 className="mb-4" style={{ fontSize: '20px', fontWeight: 'bold'}} >FECHA Y HORA</h2>
-        <p className="mb-4">10-10-23 11:05</p>
-        <p className="mb-4">10-10-23 15:05</p>
-        <p className="mb-4">11-10-23 20:05</p>
-      </div>
-
-      
-      <div className="p-4">
-        <h2 className="mb-4" style={{ fontSize: '20px', fontWeight: 'bold'}} >VALOR</h2>
-        <p className="mb-4">$1000</p>
-        <p className="mb-4">$1552</p>
-        <p className="mb-4">$548182</p>
-      </div>
-
-     
-      <div className="p-4">
-        <h2 className="mb-4" style={{ fontSize: '20px', fontWeight: 'bold'}} >ESTADO</h2>
-        <p className="mb-4"> Por liquidar</p>
-        <p className="mb-4">Por liquidar</p>
-        <p className="mb-4">Por liquidar</p>
-      </div>
-    </div>
-    </div>
-    <div className='flex items-center justify-center gap-20' >
-            
-            <Button text='REGRESAR' onClick={() => router.back()} />
-          </div>
-        </Layout>
-
-
-        );
-    };
     
     export default EstadoDeCuentaPage;
--- pages/GestionFinanciera/GestionDeCuenta/index.tsx
+++ pages/GestionFinanciera/GestionDeCuenta/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/GestionFinanciera/Button';
+import { Button, Grid } from '@mui/material';
 import { Layout } from '@/components/GestionFinanciera/Layout';
 import { useRouter } from 'next/router';
 
@@ -8,18 +8,30 @@ const GestionDeCuentaPage = () => {
   return (
     <Layout>
       <div className='flex flex-col items-center justify-center py-0 px-4 gap-10' >
-        <Button
-          text='POLITICAS DE PAGO A LOS SOCIOS'
-          onClick={() => router.push('/GestionFinanciera/GestionDeCuenta/PoliticasDePago')}
-        />
-        <Button
-          text='ESTADO DE CUENTA'
-          onClick={() => router.push('/GestionFinanciera/GestionDeCuenta/EstadoDeCuenta')}
-        />
-        <Button
-          text='REGRESAR'
-          onClick={() => router.push('/GestionFinanciera')}
-        />
+      <Button
+            variant='contained'
+            size='large'
+            className='w-full'
+            onClick={() => router.push('/GestionFinanciera/GestionDeCuenta/PoliticasDePago')}
+          >
+            POLITICAS DE PAGO
+          </Button>
+          <Button
+            variant='contained'
+            size='large'
+            className='w-full'
+            onClick={() => router.push('/GestionFinanciera/GestionDeCuenta/EstadoDeCuenta')}
+          >
+            ESTADO DE CUENTA
+          </Button>
+          <Button
+            variant='contained'
+            size='large'
+            className='w-full'
+            onClick={() => router.push('/GestionFinanciera')}
+          >
+            REGRESAR
+          </Button>
       </div>
     </Layout>
   );
