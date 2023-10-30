f594764d44c5bdf096006e92d426b72481576582
Sneyderbg <sneyder.buitrago@udea.edu.co>
2023-10-29 21:05:36 -0500

    refactor: use environmental variable as api url

--- api/GestionFinanciera/Tarifas.ts
+++ api/GestionFinanciera/Tarifas.ts
@@ -2,9 +2,10 @@
 import { CityFeeRequest } from '@/interfaces/CityFee.interface';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/ssmu-api/';
-
-const api = axios.create({ baseURL: API_URL, timeout: 3000 });
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 3000,
+});
 
 const obtenerTarifasPorCiudad = () => {
   return api.get('/cities/names');
