f6de8b6fdf981cea126016110c8f69db29e7047e
Sneyderbg <sneyder.buitrago@udea.edu.co>
2023-10-30 10:31:40 -0500

    refactor: resolve 2 code smells

--- components/GestionFinanciera/SkeletonWrapper.tsx
+++ components/GestionFinanciera/SkeletonWrapper.tsx
@@ -24,7 +24,7 @@ export const SkeletonWrapper = ({
           height={child.props.height}
           variant='rounded'
           animation='wave'
-          className={className || 'w-1/2 h-1/4'}
+          className={className ?? 'w-1/2 h-1/4'}
         ></Skeleton>
       );
     });
