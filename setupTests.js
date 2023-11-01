ef04043833a312a0bb34e5d9d237f010536e97ad
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2023-11-01 17:32:56 -0500

    add test setup for compatibility with sonarcloud

---
+++ setupTests.js
@@ -0,0 +1,18 @@
+// Import the jest-dom matchers
+import '@testing-library/jest-dom/extend-expect';
+
+// Any global setup logic can go here, for example:
+// Setting up a mock localStorage
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+
+global.localStorage = localStorageMock;
+
+// You can also set up global variables
+global.myGlobalVariable = 'some value';
+
+// Or set up environment variables
+process.env.MY_ENV_VAR = 'testValue';
