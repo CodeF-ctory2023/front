b7d03c775046f8ed39ca7b4e1cead955f89a98fb
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2023-11-06 18:46:20 -0500

    Update newTestFunctionality.tsx

--- components/ExampleComponent/newTestFunctionality.tsx
+++ components/ExampleComponent/newTestFunctionality.tsx
@@ -1,7 +1,7 @@
 // newTestFunctionality.tsx
-export function sillyTestFunction(input: string | null): string {
+export const sillyTestFunction = (input: string | null): string => {
     if (input === null) {
         throw new Error('Input cannot be null');
     }
     return `Silly output: ${input}`;
-}
+};
