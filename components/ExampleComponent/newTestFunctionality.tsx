1d16792f88e3a0be6bcdc499a33573c12ac89f51
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2023-11-06 18:31:21 -0500

    fixed typo

--- components/ExampleComponent/newTestFunctionality.tsx
+++ components/ExampleComponent/newTestFunctionality.tsx
@@ -1,5 +1,5 @@
-// newFunctionality.tsx
-export function sillyFunction(input: string | null): string {
+// newTestFunctionality.tsx
+export function sillyTestFunction(input: string | null): string {
     if (input === null) {
         throw new Error('Input cannot be null');
     }
