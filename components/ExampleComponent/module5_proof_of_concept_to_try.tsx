f804e7989de68a1d55a8da85a1fd7a234f8a359c
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2023-11-06 18:39:00 -0500

    Update module5_proof_of_concept_to_try.tsx

--- components/ExampleComponent/module5_proof_of_concept_to_try.tsx
+++ components/ExampleComponent/module5_proof_of_concept_to_try.tsx
@@ -1,6 +1,5 @@
-// newFunctionality.js
-function sillyFunction(input) {
+// module5_proof_of_concept_to_try.tsx
+
+export function sillyFunction(input: string) {
     return `Silly output: ${input}`;
 }
-
-module.exports = sillyFunction;
