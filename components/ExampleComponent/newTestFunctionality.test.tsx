248d3875d2b642057bec9f2dd65bd77f10e4fa96
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2023-11-06 18:23:46 -0500

    added test for JEST  to newTestFunctionality.test.tsx

---
+++ components/ExampleComponent/newTestFunctionality.test.tsx
@@ -0,0 +1,20 @@
+// newFunctionality.test.tsx
+import { sillyFunction } from './newFunctionality';
+
+describe('sillyFunction', () => {
+    // Test for normal input
+    it('should prepend "Silly output: " to the input string', () => {
+        expect(sillyFunction('test')).toBe('Silly output: test');
+        expect(sillyFunction('123')).toBe('Silly output: 123');
+    });
+
+    // Test for empty string input
+    it('should handle an empty string', () => {
+        expect(sillyFunction('')).toBe('Silly output: ');
+    });
+
+    // Test for null input
+    it('should throw an error if input is null', () => {
+        expect(() => sillyFunction(null)).toThrow('Input cannot be null');
+    });
+});
