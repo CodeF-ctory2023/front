2b9518883aef0253d254516b785f373048e54144
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2023-11-06 18:46:31 -0500

    fixed function typo

--- components/ExampleComponent/newTestFunctionality.test.tsx
+++ components/ExampleComponent/newTestFunctionality.test.tsx
@@ -4,17 +4,17 @@ import { sillyTestFunction } from './newTestFunctionality';
 describe('sillyFunction', () => {
     // Test for normal input
     it('should prepend "Silly output: " to the input string', () => {
-        expect(sillyFunction('test')).toBe('Silly output: test');
-        expect(sillyFunction('123')).toBe('Silly output: 123');
+        expect(sillyTestFunction('test')).toBe('Silly output: test');
+        expect(sillyTestFunction('123')).toBe('Silly output: 123');
     });
 
     // Test for empty string input
     it('should handle an empty string', () => {
-        expect(sillyFunction('')).toBe('Silly output: ');
+        expect(sillyTestFunction('')).toBe('Silly output: ');
     });
 
     // Test for null input
     it('should throw an error if input is null', () => {
-        expect(() => sillyFunction(null)).toThrow('Input cannot be null');
+        expect(() => sillyTestFunction(null)).toThrow('Input cannot be null');
     });
 });
