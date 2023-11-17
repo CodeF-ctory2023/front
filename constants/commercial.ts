abc9fdbdad02883813a6afe70ea8f9d749ab4763
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-11-17 11:09:13 -0500

    feat: :sparkles: Add constants for user types and regions

---
+++ constants/commercial.ts
@@ -0,0 +1,30 @@
+export const COMMERCIAL_CONSTANTS = {
+  userTypes: [
+    {
+      id: 'FRECUENTE',
+      name: 'Frecuente',
+    },
+    {
+      id: 'OCASIONAL',
+      name: 'Ocasional',
+    },
+  ],
+  regions: [
+    {
+      id: 'MEDELLIN',
+      name: 'Medellín',
+    },
+    {
+      id: 'BOGOTA',
+      name: 'Bogotá',
+    },
+    {
+      id: 'CALI',
+      name: 'Cali',
+    },
+    {
+      id: 'BARRANQUILLA',
+      name: 'Barranquilla',
+    },
+  ],
+};
