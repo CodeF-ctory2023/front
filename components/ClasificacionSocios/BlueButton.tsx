21132406751f159ec6ed5a2773ed3ed870de3d8c
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-11-19 19:00:22 -0500

    feat: Add HTTP Post to create groups

--- components/ClasificacionSocios/BlueButton.tsx
+++ components/ClasificacionSocios/BlueButton.tsx
@@ -1,12 +1,15 @@
 interface BlueButtonProps {
     text?: string;
+    onClick?: () => void;
 }
 
-const BlueButton = ({ text = "Blue Button" }: BlueButtonProps) => {
+const BlueButton = ({ text = "Blue Button", onClick }: BlueButtonProps) => {
     return (
 
         <div className="flex gap-3 items-center">
-            <button className="text-white px-4 py-3 rounded-xl border-none cursor-pointer bg-blue-500 hover:bg-blue-400  hover:duration-150">
+            <button 
+            className="text-white px-4 py-3 rounded-xl border-none cursor-pointer bg-blue-500 hover:bg-blue-400  hover:duration-150"
+            onClick={onClick}>
                 {text}
             </button>
         </div>
--- components/ClasificacionSocios/MainConfigurarCriterios.tsx
+++ components/ClasificacionSocios/MainConfigurarCriterios.tsx
@@ -1,7 +1,33 @@
+import axios from "axios";
 import { BlueButton } from "./BlueButton";
+import { useState } from "react";
 
 const MainConfigurarCriterios = () => {
 
+    const [nombreGrupo, setNombreGrupo] = useState("");
+    const [descripcionGrupo, setDescripcionGrupo] = useState("");
+    const [criterioClasificacion, setCriterioClasificacion] = useState("");
+    const [valorCriterio, setValorCriterio] = useState("");
+    
+
+    const crearGrupo = async () => {
+        try {
+          await axios.post("http://localhost:8080/api/grupo/crearGrupo", {
+            idGrupo: 13,
+            nombreGrupo: nombreGrupo,
+            descripcion: descripcionGrupo,
+            criterio: criterioClasificacion,
+            valorCriterio: parseInt(valorCriterio, 10), // Asegúrate de convertir a número si es necesario
+          });
+    
+          // Resto del código si la solicitud es exitosa
+    
+        } catch (error) {
+          console.error(error);
+          window.alert("Error al crear el grupo");
+        }
+      };
+      
     return (
 
         <div className="flex flex-col px-5 py-5 w-full font-m2">
@@ -14,29 +40,52 @@ const MainConfigurarCriterios = () => {
                 <div className="grid grid-cols-2 gap-x-12 gap-y-20">
 
                     <div className="flex flex-col">
-                        <span>Nombre de grupo</span>
-                        <input className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" type="text" placeholder="Type here" />
+                        <span>Nombre del grupo</span>
+                        <input 
+                        className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" 
+                        type="text" 
+                        placeholder="Type here" 
+                        value={nombreGrupo}
+                        onChange={(e) => setNombreGrupo(e.target.value)}/>
                     </div>
 
                     <div className="flex flex-col">
-                        <span>Nombre de grupo</span>
-                        <input className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" type="text" placeholder="Type here" />
+                        <span>Descripcion del grupo</span>
+                        <input 
+                        className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" 
+                        type="text" 
+                        placeholder="Type here" 
+                        value={descripcionGrupo}
+                        onChange={(e) => setDescripcionGrupo(e.target.value)}
+                        />
                     </div>
 
                     <div className="flex flex-col">
-                        <span>Nombre de grupo</span>
-                        <input className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" type="text" placeholder="Type here" />
+                        <span>Criterio de clasificacion</span>
+                        <input 
+                        className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" 
+                        type="text" 
+                        placeholder="Type here" 
+                        value={criterioClasificacion}
+                        onChange={(e) => setCriterioClasificacion(e.target.value)}
+                        />
                     </div>
 
                     <div className="flex flex-col">
-                        <span>Nombre de grupo</span>
-                        <input className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" type="text" placeholder="Type here" />
+                        <span>Valor criterio</span>
+                        <input 
+                        className="w-96 h-16 rounded-lg border border-gray-400 py-4 px-4" 
+                        type="text" 
+                        placeholder="Type here"
+                        value={valorCriterio}
+                        onChange={(e) => setValorCriterio(e.target.value)}
+                        />
                     </div>
 
                 </div>
 
                 <div className="flex justify-evenly mt-60 w-full">
-                    <BlueButton text="Guardar Información" />
+                    <BlueButton text="Guardar Información" onClick={crearGrupo} />
                     <BlueButton text="Reiniciar Proceso" />
                 </div>
 
